70cd3b7e5dd38b637bae3706dfeb95c845d42621
Enes Esen <enesen@gmx.com>
2022-07-24 13:49:39 +0300

    update: brew & vsc extensions

--- packages/vscode-extensions.sh
+++ packages/vscode-extensions.sh
@@ -1,33 +1,55 @@
-code --install-extension christian-kohler.path-intellisense
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension eamodio.gitlens
-code --install-extension EditorConfig.EditorConfig
-code --install-extension mikestead.dotenv
-code --install-extension ms-python.python
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension naumovs.color-highlight
-code --install-extension skyapps.fish-vscode
-code --install-extension vincaslt.highlight-matching-tag
+###############################################################################
+# VSCode Extensions installation scripts                                      #
+###############################################################################
+
+# export current vscode extension list
+# code --list-extensions | xargs -L 1 echo code --install-extension           #
+
 code --install-extension aaron-bond.better-comments
 code --install-extension amatiasq.sort-imports
 code --install-extension anseki.vscode-color
 code --install-extension apollographql.vscode-apollo
+code --install-extension bradlc.vscode-tailwindcss
 code --install-extension burkeholland.simple-react-snippets
 code --install-extension byCedric.vscode-expo
 code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension ckolkman.vscode-postgres
 code --install-extension codezombiech.gitignore
 code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension ecmel.vscode-html-css
+code --install-extension EditorConfig.EditorConfig
 code --install-extension eg2.vscode-npm-script
 code --install-extension esbenp.prettier-vscode
 code --install-extension GraphQL.vscode-graphql
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension jock.svg
+code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension kamikillerto.vscode-colorize
+code --install-extension mhutchie.git-graph
+code --install-extension mikestead.dotenv
 code --install-extension mrmlnc.vscode-scss
+code --install-extension ms-ossdata.vscode-postgresql
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension msjsdiag.vscode-react-native
+code --install-extension mtxr.sqltools
+code --install-extension oderwat.indent-rainbow
 code --install-extension pflannery.vscode-versionlens
 code --install-extension Prisma.prisma
+code --install-extension skyapps.fish-vscode
+code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension svelte.svelte-vscode
+code --install-extension toba.vsfire
+code --install-extension vincaslt.highlight-matching-tag
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vivaxy.vscode-conventional-commits
 code --install-extension vscode-icons-team.vscode-icons
+code --install-extension WallabyJs.quokka-vscode
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension zhuangtongfa.material-theme
-code --install-extension gruntfuggly.todo-tree
-code --install-extension streetsidesoftware.code-spell-checker
